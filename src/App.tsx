f0322f7527a202cbfd1e3392c0510a5b702ec79f
gabrielvaloriani <gabriel.valoriani@gmail.com>
2024-05-01 11:05:07 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Quiz from './components/Quiz/Quiz'
 import Home from "./components/Home/Home"
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/Quiz' element={<Quiz/>}></Route>
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/Quiz' element={<Quiz/>}></Route>
+      </Routes>
+    </Router>
   )
 }
 
