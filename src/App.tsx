b70de8ce1ca95b0b6f62713af6d2e1d191fc9b1a
gabrielvaloriani <gabriel.valoriani@gmail.com>
2024-05-01 10:50:15 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Quiz from './components/Quiz/Quiz'
 import Home from "./components/Home/Home"
 
